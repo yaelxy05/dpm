f538f714ce3fa43abdea2b28dfe6db615547b6b1
yaelx <yaelx@localhost>
2021-07-20 00:55:49 +0200

    add style home card-product

--- public/content/themes/dpm/index.php
+++ public/content/themes/dpm/index.php
@@ -6,17 +6,23 @@ get_header();
 
 
 <main class="main">
-    <!-- loop for display post product -->
-    <?php while(have_posts()): the_post(); ?>
     
-    <div class="main_wrapper">
-        <!-- display of picture -->
-        <?php the_post_thumbnail('post-thumbnail', ['class' => 'main_img', 'alt'=> 'article-product'])?> 
-        <h1 class="main_h1"><?php the_title() ?></h1>
-        <!-- diplay of excerpt -->
-        <?php the_excerpt('post-excerpt', ['class' => 'main_p']) ?>
+    <div class="main_box">
+        <h1 class="main_h1">Nouveautés</h1>
+        <div class="main_post">
+        <!-- loop for display post product -->
+        <?php while(have_posts()): the_post(); ?>
+    
+            <div class="main_wrapper">
+                <!-- display of picture -->
+                <?php the_post_thumbnail('post-thumbnail', ['class' => 'main_img', 'alt'=> 'article-product'])?> 
+                <h2 class="main_h2"><?php the_title() ?></h2>
+                <!-- diplay of excerpt -->
+                <?php the_excerpt('post-excerpt', ['class' => 'main_p']) ?>
+            </div>
+        <?php endwhile ?>
+        </div>
     </div>
-    <?php endwhile ?>
 </main>
 
 <?php
