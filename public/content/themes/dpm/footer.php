d63faaab25448dc2eed9a74411873181ab8e56df
MarineRaudin <raudin.marine@gmail.com>
2021-07-19 11:41:10 +0200

    footer content added

--- public/content/themes/dpm/footer.php
+++ public/content/themes/dpm/footer.php
@@ -1,3 +1,17 @@
+<footer class="section footer"> 
+
+    <img class="footer-logo" src="content/themes/dpm/assets/images/IMG_2820.png" alt="logo">
+
+    <nav>
+        <ul>
+            <li><a href="#">Mentions légales</a></li>
+            <li><a href="#">A propos</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+
+</footer>  
+    
     <?php
     wp_footer();
     ?>
