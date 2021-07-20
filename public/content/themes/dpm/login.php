84be9dbbe45b73c115a92a4647eced5bd97fb712
MarineRaudin <raudin.marine@gmail.com>
2021-07-20 14:16:31 +0200

    form added but not dynamic

--- public/content/themes/dpm/login.php
+++ public/content/themes/dpm/login.php
@@ -4,7 +4,24 @@ get_header();
 
 <h1>Connexion</h1>
 
+<form action="" method="get" class="login-form">
+  <div">
+    <label for="name">Nom d'utilisateur</label>
+    <input type="text" name="name" id="name" required>
+  </div>
+  <div">
+    <label for="email">Mot de passe</label>
+    <input type="email" name="email" id="email" required>
+  </div>
+  <div">
+    <input type="submit" value="Connexion">
+  </div>
+</form>
 
+<div>
+    <a href="url">Mot de passe oublié</a>
+    <a href="url">Pas encore inscrit?</a>
+</div>
 
 <?php
 get_footer();
