9701f797b86a4eec020ff58366488c2731554c18
MarineRaudin <raudin.marine@gmail.com>
2021-07-21 12:18:23 +0200

    initalisation page fille

--- public/content/themes/dpm/girl-clothing-template.php
+++ public/content/themes/dpm/girl-clothing-template.php
@@ -8,7 +8,27 @@
 get_header();
 ?>
 
+<main class="main">
+    
+    <div class="main_box">
+        <h1 class="main_h1">Filles</h1>
+        <div class="main_post">
 
+        <!-- loop for display post product -->
+        <?php while(have_posts()): the_post(); ?>
+    
+            <div class="main_wrapper">
+                <!-- display of picture -->
+                <?php the_post_thumbnail('post-thumbnail', ['class' => 'main_img', 'alt'=> 'article-product'])?> 
+                <h2 class="main_h2"><?php the_title() ?></h2>
+                <!-- diplay of excerpt -->
+                <?php the_field('prix', ['class' => 'main_p']) ?>
+            </div>
+        <?php endwhile ?>
+
+        </div>
+    </div>
+</main>
 
 
 <?php
--- public/content/themes/dpm/page.php
+++ public/content/themes/dpm/page.php
@@ -3,7 +3,10 @@ get_header();
 ?>
 
 <main class="main">
-    <div class="main_block">Coucou</div>
+    <div class="main_block">
+    <?php the_title(); ?>
+    <?php the_content(); ?>
+    </div>
 </main>
 <div class="test1"></div>
 
