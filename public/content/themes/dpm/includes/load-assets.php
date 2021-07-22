66c1989c62b36cf50c2c7f735735c6bf158d0e14
yaelx <yaelx@localhost>
2021-07-22 16:09:46 +0200

    factorisation of function wp + update home link

--- public/content/themes/dpm/includes/load-assets.php
+++ public/content/themes/dpm/includes/load-assets.php
@@ -29,42 +29,6 @@ function dpm_load_assets()
     );
 }
 
-// ==========================================================================================
-// display title tag 
-// ==========================================================================================
-function dpm_supports() 
-{
-    add_theme_support('title-tag');
-    add_theme_support('post-thumbnails');
-    add_theme_support('menus');
-    register_nav_menu('header', 'en tête du menu');
-    register_nav_menu('header-mobile', 'en tête du menu mobile');
-}
 
-function dpm_menu_class($classes) 
-{
-    $classes[]= 'nav-items';
-    return $classes;
-}
-
-function dpm_menu_link_class($attrs)
-{
-    $attrs['class'] = 'header_link';
-    return $attrs;
-}
-
-/**
- * Add icons
- */
-add_filter( 'wp_nav_menu_items', 'nav_menu_add_search', 10, 2 );
-function nav_menu_add_search( $items, $args ) {
-	if ( in_array($args->theme_location,['header','header-mobile'])) {
-		$menulink = '<li><i class="fas fa-shopping-cart header_link"></i></li>';
-		$items .= $menulink;
-	}
-	return $items;
-}
 add_action('wp_enqueue_scripts', 'dpm_load_assets');
-add_action('after_setup_theme','dpm_supports');
-add_filter('nav_menu_css_class', 'dpm_menu_class');
-add_filter('nav_menu_link_attributes', 'dpm_menu_link_class');
+
