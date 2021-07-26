da88a976e3b582c69936ea0d324c7a231c80f9cb
yaelx <yaelx@localhost>
2021-07-26 19:11:34 +0200

    add menu active in javascript and add function wp special_classes

--- public/content/themes/dpm/assets/js/menu-active.js
+++ public/content/themes/dpm/assets/js/menu-active.js
@@ -3,9 +3,9 @@ console.log('%c' + "Fichier menu-active.js chargé", 'color: #0bf; font-size: 1r
 const menu = {
     menuSelectors:null,
     init: function() {
-
+        // we target the the class menu-item
         menu.menuSelectors = document.querySelectorAll('.menu-item');
-        
+        // we go through the whole class and we listen to the click on the menu
         for(let menuSelector of menu.menuSelectors) {
             menuSelector.addEventListener('click', menu.handleClick);
         }
@@ -17,20 +17,19 @@ const menu = {
 
         
         // DOC JS event.currentTarget https://developer.mozilla.org/en-US/docs/Web/API/Event/currentTarget
+        // we create a variable for to recover the current menu
         const clickedButton = event.currentTarget;
         console.log(clickedButton);
+        // we give the result of clickedButton to the function setCurrentActive in callback
         menu.setCurrentActive(clickedButton);
     },
 
     setCurrentActive:function(clickedButton) {
+        // we target the current element who have the class active
         const currentLink = document.querySelector('.menu-item.active');
-        
-            currentLink.classList.remove('active');
-        
-        
-            clickedButton.classList.add('active');
-        
-        
+        // if he have the class active we remove the classe active and else we add to the class menu-item
+        currentLink.classList.remove('active');
+        clickedButton.classList.add('active');
     }
 };
 
