17843a18e1071fe714be5aee27bd8e9bf365e19a
yaelx <yaelx@localhost>
2021-07-26 12:21:28 +0200

    add page login +css

--- public/content/themes/dpm/header.php
+++ public/content/themes/dpm/header.php
@@ -19,7 +19,7 @@
         <div class="header_wrapper">
             <div class="header_login">
                 <ul class="header_login-li">
-                    <li>Se connecter</li>
+                    <li><a href=”http://localhost/apo/il-etait-plusieurs-doigts/public/connexion/”>Se connecter</a></li>
                     <li>/</li>
                     <li> Créer un compte</li>
                 </ul>
