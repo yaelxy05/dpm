33faede30b9f68c7483573bf97a49318b43345c3
yaelx <yaelx@localhost>
2021-07-28 14:44:52 +0200

    init branch

---
+++ public/content/themes/dpm/templates/contact.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Template Name: Contact
+ */
+?>
+<?php
+get_header();
+?>
+
+<main class="main">
+        <h1 class="main_h1">Nous contactez</h1>
+
+       
+    </main>
+
+<?php
+get_footer();
+?>
