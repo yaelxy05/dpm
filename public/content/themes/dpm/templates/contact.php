9d105e2a5ce2c0b5ee7052e4ea5636e25ef61db7
yaelx <yaelx@localhost>
2021-07-28 15:10:22 +0200

    add page contact

--- public/content/themes/dpm/templates/contact.php
+++ public/content/themes/dpm/templates/contact.php
@@ -10,9 +10,27 @@ get_header();
 
 <main class="main">
         <h1 class="main_h1">Nous contactez</h1>
-
-       
-    </main>
+        <div class="contact_box">
+            <div class="contact_member">
+                <img src="<?= get_theme_file_uri('assets/images/T01BJVA2D54-U01BYU3V7RD-6f43116e4d05-512.png') ?>" alt="">
+                <p>HUE Yaël</p>
+                <p>Scrum-master</p>
+                <a href="https://www.linkedin.com/in/hueyael/"><i class="fab fa-linkedin icon"></i></a>
+            </div>
+            <div class="contact_member">
+                <img src="" alt="">
+                <p></p>
+            </div>
+            <div class="contact_member">
+                <img src="" alt="">
+                <p></p>
+            </div>
+            <div class="contact_member">
+                <img src="" alt="">
+                <p></p>
+            </div>
+        </div>
+</main>
 
 <?php
 get_footer();
