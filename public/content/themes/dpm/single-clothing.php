f8c350d8f086297b5fc40aebd47c884d4d322039
AxelleBodin <bodin.axelle@hotmail.fr>
2021-07-28 15:28:23 +0200

    essai panier

--- public/content/themes/dpm/single-clothing.php
+++ public/content/themes/dpm/single-clothing.php
@@ -1,15 +1,32 @@
 <?php get_header();?>
 
 
-<?php
 
- while(have_posts()): the_post(); ?>
-    <?php the_post_thumbnail('post-thumbnail', ['class' => 'main_img', 'alt'=> 'article-product'])?> 
-    <br>
-    <?php the_title(); ?>
-    <?php the_excerpt('post-excerpt', ['class' => 'main_p']) ?>
-    <br>
-    <?php the_field('prix'); ?>€
-<?php endwhile ?>
+    <main class="main">
+        <h1 class="main_h1"><?php the_title(); ?></h1>
+
+        
+            <div class="single_box">
+                <div class="single_img-box">
+                    <?php the_post_thumbnail('post-thumbnail', ['class' => 'single_img', 'alt'=> 'article-product'])?> 
+                </div>
+                <div class="single_description">
+                    <p class="single_age"><?php the_category() ?>1Mois</p>
+                    <p class="single_category"><?php the_category(); ?>Bébé</p>
+                    <p class="single_price"><?php the_field('prix'); ?> €</p>
+                    <button><?php get_submit_button(
+                        $text = 'Ajouter au panier',
+                        $name = 'submit'); ?>Ajouter au panier</button>
+
+                </div>
+                <div class="single_description--full">
+                    <h2>Description</h2>
+                    <p class="single_excerpt"><?php the_excerpt('post-excerpt') ?></p>
+                </div>
+            </div>
+       
+    </main>
+
+
 
 <?php get_footer();?>
