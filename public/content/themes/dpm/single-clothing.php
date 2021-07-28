c010f066673736d21e02ccf03c76f98def03e589
yaelx <yaelx@localhost>
2021-07-28 12:14:59 +0200

    add single clothing style

--- public/content/themes/dpm/single-clothing.php
+++ public/content/themes/dpm/single-clothing.php
@@ -1,15 +1,26 @@
 <?php get_header();?>
 
 
-<?php
 
- while(have_posts()): the_post(); ?>
-    <?php the_post_thumbnail('post-thumbnail', ['class' => 'main_img', 'alt'=> 'article-product'])?> 
-    <br>
-    <?php the_title(); ?>
-    <?php the_excerpt('post-excerpt', ['class' => 'main_p']) ?>
-    <br>
-    <?php the_field('prix'); ?>€
-<?php endwhile ?>
+    <main class="main">
+        <h1 class="main_h1"><?php the_title(); ?></h1>
+
+        <?php while (have_posts()) : the_post(); ?>
+            <div class="single_box">
+                <div class="single_img-box">
+                    <?php the_post_thumbnail('post-thumbnail', ['class' => 'single_img', 'alt'=> 'article-product'])?> 
+                </div>
+                <div class="single_description">
+                    <p class="single_name"><?php the_title(); ?></p>
+                    <p class="single_price"><?php the_field('prix'); ?>€</p>
+                    <p class="single_age"><?= the_category() ?></p>
+                    <p class="single_category"><?= the_field('age'); ?></p>
+                    <button>Ajouter au panier</button>
+                </div>
+            </div>
+        <?php endwhile ?>
+    </main>
+
+
 
 <?php get_footer();?>
