22ce67622172487d21572fa4f68ebf16e5ca95c3
yaelx <yaelx@localhost>
2021-07-29 10:22:20 +0200

    add condition is login

--- public/content/themes/dpm/header.php
+++ public/content/themes/dpm/header.php
@@ -19,9 +19,14 @@
         <div class="header_wrapper">
             <div class="header_login">
                 <ul class="header_login-li">
-                    <li><a href="<?= wp_login_url(); ?>">Se connecter</a></li>
-                    <li>/</li>
-                    <li><a href="<?= wp_registration_url(); ?>"> Créer un compte</a></li>
+                    <?php if ( is_user_logged_in() ) { ?>
+                        <li><a href="http://localhost/apo/il-etait-plusieurs-doigts/public/espace-utilisateur/">Espace Utilisateur</a></li>
+                        <li><a href="<?php echo wp_logout_url(); ?>">Déconnexion</a></li>
+                    <?php } else { ?>
+                        <li><a href="<?= wp_login_url(); ?>">Se connecter</a></li>
+                        <li>/</li>
+                        <li><a href="<?= wp_registration_url(); ?>"> Créer un compte</a></li>
+                    <?php } ?>
                 </ul>
             </div>
             <div class="header_nav">
