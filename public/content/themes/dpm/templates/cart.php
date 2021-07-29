9a94a86cb60ba0db3828cbe9c984de50d04b6134
AxelleBodin <bodin.axelle@hotmail.fr>
2021-07-29 14:46:28 +0200

    dynamisation of cart

--- public/content/themes/dpm/templates/cart.php
+++ public/content/themes/dpm/templates/cart.php
@@ -13,10 +13,9 @@ $cartProducts = $cartModel->getUserCart(get_current_user_id());
     <div class="cart_wrapper">
         <?php foreach($cartProducts as $post) : setup_postdata($post); ?>
             <div class="cart_product">
-                <img class="cart_picture" src="<?= get_theme_file_uri('assets/images/accessoires/206094083_343378580660736_3262409337469414675_n.jpg') ?>" alt="cart product">
+                <img class="cart_picture" src="<?php the_post_thumbnail('post-thumbnail', ['class' => 'single_img', 'alt'=> 'article-product'])?>
                 <p class="cart_description"><?php the_title(); ?></p>
-                <p class="cart_quantity">1</p>
-                <p class="cart_price">9.99€</p>
+                <p class="cart_price"><?php the_field('prix'); ?> €</p>
                 <i class="fas fa-trash-alt"></i>
             </div>
         <?php endforeach; wp_reset_postdata(); ?>
@@ -29,7 +28,7 @@ $cartProducts = $cartModel->getUserCart(get_current_user_id());
         </div>
         <div class="cart_total-product">
             <p>Livraison</p>
-            <p>40€</p>
+            <p>3.90€</p>
         </div>
         <div class="cart_total-product">
             <p>Total</p>
