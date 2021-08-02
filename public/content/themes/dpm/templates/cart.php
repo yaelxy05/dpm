beddc0817ad301a838d3671a3ad273cfa6ec8902
yaelx <yaelx@localhost>
2021-08-02 10:14:59 +0200

    add total cart

--- public/content/themes/dpm/templates/cart.php
+++ public/content/themes/dpm/templates/cart.php
@@ -11,7 +11,10 @@ $cartProducts = $cartModel->getUserCart(get_current_user_id());
 <main class="main">
     <h1 class="main_h1">Panier</h1>
     <div class="cart_wrapper">
-        <?php foreach($cartProducts as $post) : setup_postdata($post); ?>
+
+        <?php $total_price = 0; ?>
+        <?php foreach($cartProducts as $post) : setup_postdata($post); 
+        $total_price += get_field('prix'); ?>
             <div class="cart_product">
                 <img class="cart_picture" src="<?php the_post_thumbnail('post-thumbnail', ['class' => 'single_img', 'alt'=> 'article-product']);?>
                 <p class="cart_description"><?php the_title(); ?></p>
@@ -24,7 +27,7 @@ $cartProducts = $cartModel->getUserCart(get_current_user_id());
     <div class="cart_total">
         <div class="cart_total-product">
             <p>Produits</p>
-            <p>40€</p>
+            <p><?= $total_price; ?>€</p>
         </div>
         <div class="cart_total-product">
             <p>Livraison</p>
@@ -32,7 +35,7 @@ $cartProducts = $cartModel->getUserCart(get_current_user_id());
         </div>
         <div class="cart_total-product">
             <p>Total</p>
-            <p>40€</p>
+            <p><?= $total_price + 3.90; ?>€</p>
         </div>
         <div class="cart_total-button">
             <button>Valider ma commande</button>
