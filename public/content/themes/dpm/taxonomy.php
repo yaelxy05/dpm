67db43bdbbe576fdd43aafcc2ddc29b75d573d92
MarineRaudin <raudin.marine@gmail.com>
2021-08-03 22:47:54 +0200

    le retour de la sidebar mais en mieux mais pas responsive

--- public/content/themes/dpm/taxonomy.php
+++ public/content/themes/dpm/taxonomy.php
@@ -3,10 +3,10 @@ get_header();
 ?>
 
 <main class="main">
-    <div class="display-sidebar">
-        <div class="main_box">
-            <h1 class="main_h1"><?php single_cat_title('', true) ?></h1>
 
+    <div class="main_box">
+        <h1 class="main_h1"><?php single_cat_title('', true) ?></h1>
+        <div class="display-sidebar">
             <div class="sidebar">
                 <form>
                     <div class="container">
@@ -52,6 +52,15 @@ get_header();
 
                         </div>
 
+                        <button class="accordion">Couleur</button>
+                        <div class="accordion-content">
+                            <ul class="accordion_box">
+                                <li><input type="checkbox" name="type[]" value="hauts-t-shirt">Hauts – T-shirts</li>
+
+                            </ul>
+
+                        </div>
+
 
                         <button class="filter-button">Filtrer</button>
 
@@ -60,33 +69,29 @@ get_header();
                 </form>
             </div>
 
+            <div class="main_post">
+
+                <!-- loop for display post product -->
+                <?php while (have_posts()) : the_post(); ?>
+
+                    <div class="main_wrapper">
+                        <!-- display of picture -->
+                        <?php the_category() ?>
+                        <a href="<?php the_permalink() ?>"><?php the_post_thumbnail('post-thumbnail', ['class' => 'main_img', 'alt' => 'article-product']) ?>
+                            <h2 class="main_h2"><?php the_title() ?> </h2>
+                            <div class="main_detail-box">
+                                <a href="<?php the_permalink() ?>">
+                                    <p class="detail">Afficher les détails</p>
+                                </a>
+                                <!-- diplay of excerpt -->
+
+                                <p class="prix"><?php the_field('prix') ?> €</p>
+                            </div>
+                    </div>
+                <?php endwhile ?>
+            </div>
         </div>
     </div>
-
-
-    <div class="main_box">
-        <div class="main_post">
-
-            <!-- loop for display post product -->
-            <?php while (have_posts()) : the_post(); ?>
-
-                <div class="main_wrapper">
-                    <!-- display of picture -->
-                    <?php the_category() ?>
-                    <a href="<?php the_permalink()?>"><?php the_post_thumbnail('post-thumbnail', ['class' => 'main_img', 'alt' => 'article-product']) ?>
-                    <h2 class="main_h2"><?php the_title() ?> </h2>
-                    <div class="main_detail-box">
-                        <a href="<?php the_permalink() ?>">
-                            <p class="detail">Afficher les détails</p>
-                        </a>
-                        <!-- diplay of excerpt -->
-
-                        <p class="prix"><?php the_field('prix') ?> €</p>
-                    </div>
-                </div>
-            <?php endwhile ?>
-
-        </div>
     </div>
 
 
