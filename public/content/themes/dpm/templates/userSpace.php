1208f5fe471c5258e1562793acde6e184f940757
yaelx <yaelx@localhost>
2021-08-03 16:43:54 +0200

    fix problem function && display loop user product

--- public/content/themes/dpm/templates/userSpace.php
+++ public/content/themes/dpm/templates/userSpace.php
@@ -8,14 +8,31 @@
 get_header();
 ?>
 
-    <main class="main">
+<main class="main">
         <h1 class="main_h1">Mon profil</h1>
         <div class="main_wrapper">
             <h2 class="main_h2">Mes articles en vente</h2>
+<?php
+$user_id = get_current_user_id();
+$loop = new WP_Query( array( 'post_type' => 'clothing', 'author' => $user_id ) ); 
+if ( $loop->have_posts() ) : while ( $loop->have_posts() ) : $loop->the_post(); ?>
+    
             <div class="main_sale-box">
-                <img src="<?= get_theme_file_uri('assets/images/accessoires/208759369_2806360789676012_1529577717722661111_n.jpg') ?>" alt="picture in sale">
-                <p>prix</p>
-            </div>
+                <?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
+
+                    <h2><?php echo get_the_title(); ?></h2>
+                    <a href="<?php the_permalink(); ?>" title="<?php htmlentities(get_the_title()); ?>">
+                        <?php the_post_thumbnail(array(120,120)); // Declare pixel size you need inside the array ?>
+                    </a>
+                    <p><?php the_field('prix'); ?> €</p>
+                <p><?php the_content(); ?></p>
+                <?php endif; ?>
+            </div>          
+        
+    <?php endwhile;
+endif;
+wp_reset_postdata();
+?>
             <a href=" http://localhost/apo/il-etait-plusieurs-doigts/public/create-post/"><button>+ Ajoutez un produit</button></a>
             <div class="main_dataUser">
                 <h2 class="main_info">Informations du compte</h2>
@@ -26,9 +43,10 @@ get_header();
                     <p>Adresse</p>
                 </div>
             </div>
-        </div>
+    </div>
         
-    </main>
+</main>
+
 <?php
 get_footer();
 ?>
