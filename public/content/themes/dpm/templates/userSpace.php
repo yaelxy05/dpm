d8ff0bcd310b0f82492eef1f3ee94c3733727ea4
yaelx <yaelx@localhost>
2021-08-03 17:47:36 +0200

    add css userpsace page

--- public/content/themes/dpm/templates/userSpace.php
+++ public/content/themes/dpm/templates/userSpace.php
@@ -10,8 +10,9 @@ get_header();
 
 <main class="main">
         <h1 class="main_h1">Mon profil</h1>
-        <div class="main_wrapper">
+        
             <h2 class="main_h2">Mes articles en vente</h2>
+        <div class="main_wrapper-user">    
 <?php
 $user_id = get_current_user_id();
 $loop = new WP_Query( array( 'post_type' => 'clothing', 'author' => $user_id ) ); 
@@ -20,20 +21,21 @@ if ( $loop->have_posts() ) : while ( $loop->have_posts() ) : $loop->the_post(); 
             <div class="main_sale-box">
                 <?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
 
-                    <h2><?php echo get_the_title(); ?></h2>
+                    <h2 class="main_sale-box--h2"><?php echo get_the_title(); ?></h2>
                     <a href="<?php the_permalink(); ?>" title="<?php htmlentities(get_the_title()); ?>">
                         <?php the_post_thumbnail(array(120,120)); // Declare pixel size you need inside the array ?>
                     </a>
-                    <p><?php the_field('prix'); ?> €</p>
-                <p><?php the_content(); ?></p>
+                    <p class="main_sale-box--p"><?php the_field('prix'); ?> €</p>
                 <?php endif; ?>
             </div>          
         
     <?php endwhile;
 endif;
 wp_reset_postdata();
-?>
-            <a href=" http://localhost/apo/il-etait-plusieurs-doigts/public/create-post/"><button>+ Ajoutez un produit</button></a>
+?> 
+        </div>
+
+            <a href=" http://localhost/apo/il-etait-plusieurs-doigts/public/create-post/"><button class="main_sale-box--button">+ Ajoutez un produit</button></a>
             <div class="main_dataUser">
                 <h2 class="main_info">Informations du compte</h2>
                 <div class="main_boxP">
@@ -43,7 +45,7 @@ wp_reset_postdata();
                     <p>Adresse</p>
                 </div>
             </div>
-    </div>
+   
         
 </main>
 
